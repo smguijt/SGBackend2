fa9c62c46272b8c6223ec42b6d5bd21ea7d8941a
Sietse Guijt <smguijt@outlook.com>
2024-07-25 11:09:40 +0200

    init

---
+++ Sources/App/Sockets/WebSocketConnections.swift
@@ -0,0 +1,58 @@
+import Vapor
+
+
+final class WebSocketConnections {
+    var connections: [UUID: WebSocket] = [:]
+    let lock = Lock()
+
+    func add(_ ws: WebSocket) {
+        let id = UUID()
+        lock.withLockVoid {
+            print("WebSocketConnections:add -> id: \(id.uuidString)")
+            connections[id] = ws
+        }
+        ws.onClose.whenComplete { [weak self] _ in
+            print("WebSocketConnections:onClose")
+            self?.remove(id)
+        }
+    }
+
+    func remove(_ id: UUID) {
+        lock.withLockVoid {
+            print("WebSocketConnections:remove --> id: \(id.uuidString)")
+            connections.removeValue(forKey: id)
+        }
+    }
+
+    func closeAll() {
+        lock.withLockVoid {
+            print("WebSocketConnections:closeAll")
+            connections.values.forEach { $0.close(promise: nil) }
+            connections.removeAll()
+        }
+    }
+}
+
+struct WebSocketShutdown: LifecycleHandler {
+    func shutdown(_ application: Application) {
+        print("WebSocketConnections:WebSocketShutdown")
+        application.webSocketConnections.closeAll()
+    }
+}
+
+extension Application {
+    
+    private struct WebSocketConnectionsKey: StorageKey {
+        typealias Value = WebSocketConnections
+    }
+
+    var webSocketConnections: WebSocketConnections {
+        if let existing = storage[WebSocketConnectionsKey.self] {
+            return existing
+        } else {
+            let new = WebSocketConnections()
+            storage[WebSocketConnectionsKey.self] = new
+            return new
+        }
+    }
+}
---
+++ Sources/App/Sockets/WebSockets.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Vapor
+
+func webSocketRoute(_ app: Application, _ socketName: String = "echo") throws {
+
+    //var activeWebSockets = Set<WebSocket>()
+    
+    app.webSocket("\(socketName)") { req, ws in
+      print("WebSocket -> \(socketName): connected")
+        
+        // add connection to the connection manager
+        // taken from the WebSocketConnections.swift file
+        app.webSocketConnections.add(ws)
+        
+        ws.onText { ws, text in
+            print("WebSocket \(socketName) -> Ontvangen bericht -> \(text)")
+            ws.send("SGBackend2.\(socketName): Ok.") // send return message
+        }
+
+        ws.onBinary { ws, data in
+            print("WebSocket \(socketName) -> Ontvangen binaire data -> \(data)")
+            //ws.send(data) // send return message
+            ws.send("SGBackend2.\(socketName): Ok.")
+        }
+
+        ws.onClose.whenComplete { result in
+            print("WebSocket \(socketName) -> verbinding gesloten -> \(result).")
+        }
+    }
+}
+    
+    
+
